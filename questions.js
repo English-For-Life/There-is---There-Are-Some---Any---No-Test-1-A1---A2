df854cfb092da9bf25dcb8283185f5bc8710df3b
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-25 09:51:50 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"...... a very good film on TV tonight. We can watch it together. Have you got ...... free time?",
+        options:["There is / any", "Are there / no", "Is there / a few", "There isn't / some"],
+        answer: 0
+    },
+    {
+        q:"Would you like ...... ice cream with your dessert? - No, thanks.",
+        options:["a", "some", "no", "a few"],
+        answer: 1
+    },
+    {
+        q:"She has got ...... friends ...... in Canada.",
+        options:["no / everywhere", "some / nowhere", "a few / somewhere", "a little / anywhere"],
+        answer: 2
+    },
+    {
+        q:"...... children are bored at the party, but you can always find ...... to entertain them.",
+        options:["A little / anything", "No / everyone", "Any / anything", "Some / something"],
+        answer: 3
+    },
+    {
+        q:"A: There are ...... students at the library at the moment. B: Yes, they are all ...... else.",
+        options:["no / somewhere", "a few / anybody", "any / everyone", "some / nothing"],
+        answer: 0
+    },
+    {
+        q:"A: There are ...... apples in the fridge. I can make an apple pie. B: Yes, but we have got ...... flour. Let's buy some.",
+        options:["no / any", "a few / no", "some / any", "any / a little"],
+        answer: 1
+    },
+    {
+     q:"There ........  ........ air conditioner in our summer house.",
+        options:["aren't / no", "is / any", "isn't / an", "are / some"],
+        answer: 2   
+    },
+    {
+        q:"....... there....... mistakes in my essay?",
+        options:["Is / a", "Are / some", "Is / a little", "Are / any"],
+        answer: 3
+    },
+    {
+        q:"He has got ...... money, so he can buy that bicycle and go ...... by bike.",
+        options:["a little / everywhere", "any / nothing", "no / anybody", "a few / somewhere"],
+        answer: 0
+    },
+    {
+        q:"There aren't ...... pickles left in the jar, but I can serve ...... else with that dish.",
+        options:["a little / anything", "any / something", "some / nothing", "no / everything"],
+        answer: 1
+    }
+]
